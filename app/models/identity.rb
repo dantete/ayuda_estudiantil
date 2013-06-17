ddb597a8200d0fbb4ecf295d1328bc014cb70e9e
Sergio <ssilvar@alumnos.utalca.cl>
2013-06-17 16:19:10 -0400

    validar email

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -2,6 +2,6 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
 	has_many :resources
 	validates_presence_of :name
 	validates_uniqueness_of :email	
-	validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+	validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@(alumnos\.)?utalca.cl$/i
 
 end
